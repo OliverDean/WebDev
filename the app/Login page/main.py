7e184cc41cd78e78b26720519c93529743b5a9e7
javelii <128959995+javelii@users.noreply.github.com>
2023-05-02 17:01:46 +0800

    Switched over from form to the chatbox

--- the app/Login page/main.py
+++ the app/Login page/main.py
@@ -1,20 +1,47 @@
-# main.py
-# How do I find the perfect match?
 import os
+import random
 import openai
+from flask import Flask, jsonify, request, render_template
 
+app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-def get_openai_response(name, age, sex, interests, nationality, humor_type, initialmeetingplace):
-    prompt = generate_prompt(name, age, sex, interests, nationality, humor_type, initialmeetingplace)
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/start", methods=["GET"])
+def start_chat():
+    prompt = f"Return the initial statement:Hi, I'm Alice and we at Reli AI understand that dating is complicated. We want to help. What is your name?"
+
+    response = openai.Completion.create(
+        model="text-davinci-003", # or whichever model you're using
+        prompt=prompt,
+        max_tokens=500,
+    )
+
+    return jsonify({
+        "message": response.choices[0].text.strip()
+    })
+
+@app.route("/chat", methods=["POST"])
+def chat():
+    user_message = request.json['message']
+
+    # Here, you should write the logic for generating a prompt based on the user's message
+    # For now, let's just use the user's message as the prompt
+    prompt = user_message
+
     response = openai.Completion.create(
         model="text-davinci-003",
         prompt=prompt,
-        temperature=0.6,
-        max_tokens=500,
+        max_tokens=60,
     )
-    return response.choices[0].text
 
-def generate_prompt(name, age, sex, interests, nationality, humor_type, initialmeetingplace):
-    prompt = f"I'm talking to {name} for the first time. They are {age} years old, {sex}, interested in {interests}, have a {humor_type} humor, and has a ethnic background of {nationality}. I want to get to know her and vice versa. What kind of story should I tell? I want to draw her in to the story slowly, and then make her laugh. I want to make them feel comfortable. Additionally, provide a one line opening line to start me of as well. I want to sound/be casual. I don't want to act as if I already know her. I don't to be attention seeking. I want the opening line to be informal and non-presumptuous"
-    return prompt
+    return jsonify({
+        "message": response.choices[0].text.strip()
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
