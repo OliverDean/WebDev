6d3113d0994e2127cd533d288ee46287fd616b99
Oliver <21307131@student.uwa.edu.au>
2023-05-20 15:53:33 +0800

    auto test

---
+++ the app/Login page/autoSelenium.py
@@ -0,0 +1,27 @@
+import time
+import subprocess
+
+# Define the number of app instances to create
+num_instances = 1
+
+# Start the loop to create and run the app instances
+for i in range(num_instances):
+    # Generate a unique port number for each instance
+    port = 5000 + i
+
+    # Start the app instance in a subprocess
+    command = f"python -m flask run -p {port}"
+    process = subprocess.Popen(command, shell=True, cwd='.')
+    print(f"Started app instance {i + 1} on port {port}...")
+    time.sleep(3)
+
+    selenium_command =f"python testing/selenium_test.py -p {port}"
+    selenium_process = subprocess.Popen(selenium_command, shell=True, cwd='.')
+    selenium_process.wait()
+
+    # Terminate the app instance subprocess
+    process.terminate()
+
+    print(f"Terminated app instance {i + 1}.")
+
+print("All app instances completed.")
