3bed8a77af19417242ff82ccf7576f6a225f86b4
Oliver <21307131@student.uwa.edu.au>
2023-05-21 13:38:17 +0800

    selenium second test

--- the app/Login page/autoSelenium.py
+++ the app/Login page/autoSelenium.py
@@ -13,6 +13,7 @@ for i in range(num_instances):
     command = f"python -m flask run -p {port}"
     process = subprocess.Popen(command, shell=True, cwd='.')
     print(f"Started app instance {i + 1} on port {port}...")
+    print(f"Starting selenium test... for registration and login")
     time.sleep(3)
 
     selenium_command =f"python testing/selenium_test.py -p {port}"
@@ -23,5 +24,26 @@ for i in range(num_instances):
     process.terminate()
 
     print(f"Terminated app instance {i + 1}.")
+
+
+    port += i
+
+    # Start the app instance in a subprocess
+    command = f"python -m flask run -p {port}"
+    process = subprocess.Popen(command, shell=True, cwd='.')
+    print(f"Started app instance {i + 1} on port {port}...")
+    print(f"Starting selenium test... for navigation")
+    time.sleep(3)
+
+    selenium_command =f"python testing/selenium2.py -p {port}"
+    selenium_process = subprocess.Popen(selenium_command, shell=True, cwd='.')
+    selenium_process.wait()
+
+    # Terminate the app instance subprocess
+    process.terminate()
+
+    print(f"Terminated app instance {i + 1}.")
+
+
 
 print("All app instances completed.")
